94c8f5dc10c165f1dd545152e37f80abfdc6bd6f
rudgnssla121@jbnu.ac.kr <71899948+khoon-git@users.noreply.github.com>
2023-04-24 06:32:45 +0900

    [dev] code_list 블러오기 / config 파일 못불러옴 내일 오류 잡기 20230424

--- tests/test_agent_ebest.py
+++ tests/test_agent_ebest.py
@@ -7,6 +7,12 @@ class TestEbest(unittest.TestCase):
     def setUp(self) -> None:
         self.ebest = EBest("DEMO")
         self.ebest.login()
+
+    def test_get_code_list(self):
+        print(inspect.stack()[0][3])
+        result = self.ebest.get_code_list("ALL")
+        assert result is not None
+        print(len(result))
     
     def tearDown(self):
         self.ebest.logout()
